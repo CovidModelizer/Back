7b28780e824883cd1a332c93d9f50ee852e268b9
mridet <mathieuridet99@gmail.com>
2021-03-06 12:14:14 +0100

    Creation projet Spring (back)

---
+++ covid-modelizer-app/src/main/java/com/inf1/covidmodelizerapp/MainApplication.java
@@ -0,0 +1,13 @@
+package com.inf1.covidmodelizerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
