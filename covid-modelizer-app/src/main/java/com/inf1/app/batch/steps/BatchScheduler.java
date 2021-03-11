28deed54fac254c2bea67e531d842ca7c17f2929
mridet <mathieuridet99@gmail.com>
2021-03-11 21:50:11 +0100

    Batch recup data + Batch scheduler

---
+++ covid-modelizer-app/src/main/java/com/inf1/app/batch/steps/BatchScheduler.java
@@ -0,0 +1,27 @@
+package com.inf1.app.batch.steps;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class BatchScheduler {
+
+	private JobLauncher jobLauncher;
+    private Job collectDataSituationReelleJob;
+    
+    // Launch batch each day at 5AM
+    @Scheduled(cron = "${batch.cron}")
+    public void schedule() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+    	jobLauncher.run(collectDataSituationReelleJob, null);
+    }
+}
--- covid-modelizer-app/src/main/java/com/inf1/app/batch/steps/RESTSituationReelleLauncher.java
+++
@@ -1,5 +0,0 @@
-package com.inf1.app.batch.steps;
-
-public class RESTSituationReelleLauncher {
-
-}
