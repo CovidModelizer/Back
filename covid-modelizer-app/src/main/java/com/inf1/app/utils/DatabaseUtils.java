08ade2e271ccc0cb1ab329a7a34057df25485adc
da-ekchajzer <da.ekchajzer@gmail.com>
2021-04-06 11:25:05 +0200

    test batch 3

--- covid-modelizer-app/src/main/java/com/inf1/app/utils/DatabaseUtils.java
+++ covid-modelizer-app/src/main/java/com/inf1/app/utils/DatabaseUtils.java
@@ -1,23 +1,18 @@
 package com.inf1.app.utils;
 
 import com.inf1.app.batch.collect_data.steps.SituationReelleProcessor;
-import com.inf1.app.jpa.dao.SituationReelleDAO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class DatabaseUtils {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SituationReelleProcessor.class);
 
-	@Autowired
-	static SituationReelleDAO sDAO;
-	
 	public static void cleanDatabase(JdbcTemplate jdbcTemplate) {
 		LOG.warn("*** Clean DB ***");
-		sDAO.deleteAll();
+		jdbcTemplate.execute("DELETE FROM situation_reelle");
 		
 		// TODO : autres tables à vider
 		// TODO : il faut pouvoir supprimer les coefficients et les indicateurs en
