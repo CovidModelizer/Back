b391005d41b86f18bc107ba4dd36f897e30470c2
da-ekchajzer <da.ekchajzer@gmail.com>
2021-04-07 19:01:38 +0200

    TU

---
+++ covid-modelizer-app/src/test/java/calculatorTest/VaccinSVIRCalculatorTest.java
@@ -0,0 +1,47 @@
+package calculatorTest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.inf1.app.batch.modelisations.calculators.VaccinSVIRCalculator;
+import com.inf1.app.dto.ModelisationDTO;
+import com.inf1.app.dto.SituationReelleDTO;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class VaccinSVIRCalculatorTest {
+
+	static List<SituationReelleDTO> situationsReelsDTO;
+
+	@BeforeAll
+	public static void init() throws IOException {
+		situationsReelsDTO = new ArrayList<SituationReelleDTO>();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		
+		SituationReelleDTO[] lines = objectMapper.readValue(new File("src/test/resources/data.json"), SituationReelleDTO[].class);
+		for(int i = 0; i<lines.length;i++) {
+			situationsReelsDTO.add(lines[i]);
+		}
+	}
+
+	@Test
+	public void test() {
+		VaccinSVIRCalculator c = new VaccinSVIRCalculator();
+		ModelisationDTO m = c.calculate(situationsReelsDTO);
+		assertEquals(m.getDateCalcul(), LocalDate.now());
+		assertEquals(m.getValues().get(LocalDate.now().plusDays(2)), "9527583");
+		assertEquals(m.getCoeff().get("beta").toString(), "0.118");
+	}
+
+}
