ab4b4226090717048e0382443c30170fc84704d8
Abdel-Benamara <abdelb.contact@gmail.com>
2021-04-10 17:56:25 +0200

    OK

--- covid-modelizer-app/src/test/java/calculatorTest/InfectionMachineLearningCalculatorTest.java
+++ covid-modelizer-app/src/test/java/calculatorTest/InfectionMachineLearningCalculatorTest.java
@@ -19,7 +19,7 @@ import com.inf1.app.dto.ModelisationDTO;
 import com.inf1.app.dto.SituationReelleDTO;
 
 public class InfectionMachineLearningCalculatorTest {
-	
+
 	static List<SituationReelleDTO> situationsReelsDTO;
 
 	@BeforeAll
@@ -27,9 +27,10 @@ public class InfectionMachineLearningCalculatorTest {
 		situationsReelsDTO = new ArrayList<SituationReelleDTO>();
 		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.registerModule(new JavaTimeModule());
-		
-		SituationReelleDTO[] lines = objectMapper.readValue(new File("src/test/resources/data.json"), SituationReelleDTO[].class);
-		for(int i = 0; i<lines.length;i++) {
+
+		SituationReelleDTO[] lines = objectMapper.readValue(new File("src/test/resources/data.json"),
+				SituationReelleDTO[].class);
+		for (int i = 0; i < lines.length; i++) {
 			situationsReelsDTO.add(lines[i]);
 		}
 	}
@@ -39,9 +40,9 @@ public class InfectionMachineLearningCalculatorTest {
 		InfectionMachineLearningCalculator c = new InfectionMachineLearningCalculator();
 		ModelisationDTO m = c.calculate(situationsReelsDTO);
 		assertEquals(m.getDateCalcul(), LocalDate.now());
-		System.out.println(m.getValues().get(LocalDate.of(2021, Month.APRIL, 9)));
-		assert(Integer.parseInt(m.getValues().get(LocalDate.of(2021, Month.APRIL, 9))) >= 53749 && Integer.parseInt(m.getValues().get(LocalDate.of(2021, Month.APRIL, 9))) <= 53751);
-		assert(m.getCoeff().get("PredJ+1_constante").toString().startsWith("-5766.63"));
+		assert (Integer.parseInt(m.getValues().get(LocalDate.of(2021, Month.APRIL, 9))) >= 53749
+				&& Integer.parseInt(m.getValues().get(LocalDate.of(2021, Month.APRIL, 9))) <= 53751);
+		assert (m.getCoeff().get("PredJ+1_constante").toString().startsWith("-5766.63"));
 	}
 
 }
